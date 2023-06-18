c80f4f16f0f29041120f997d67b2bbb84089c217
S. Tudent <student@student.local>
2023-06-19 06:19:14 +1200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+docker stop assessment2container
+docker rm assessment2container
+docker rmi --force assessmentcodeimage
 
 mkdir tempdir
 
-cp -f assessment_code.py tempdir/.
-cp -f network_equipment.json tempdir/.
+cp  assessment_code.py tempdir/.
+cp  network_equipment.json tempdir/.
 
 echo "FROM python" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  assessment_code.py /home/myapp/" >> tempdir/Dockerfile
-echo "COPY  network_equipment.json /home/myapp/" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
+echo "COPY assessment_code.py ." >> tempdir/Dockerfile
+echo "COPY network_equipment.json ." >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/assessment_code.py &" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/assessment_code.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t assessmentcodeimage .
